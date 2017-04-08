107bee3de04f6770bcfe83ba2f4f2fd6a5a837f9
Ivan <sgoidxxl@gmail.com>
2017-04-08 18:21:57 +0300

    Refactoring solution and remove unused projects

---
+++ Client/Client.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TcpClient listener = new TcpClient(new IPEndPoint(IPAddress.Any, 3210));
+            Console.WriteLine("Waiting for a server found this client...");
+
+           
+        }
+    }
+}
--- Client/Properties/AssemblyInfo.cs
+++ Client/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // Общие сведения об этой сборке предоставляются следующим набором
 // набора атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
 // связанные со сборкой.
-[assembly: AssemblyTitle("ConsoleApp")]
+[assembly: AssemblyTitle("Client")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("ConsoleApp")]
+[assembly: AssemblyProduct("Client")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
-[assembly: Guid("50a23700-6502-4bd4-9a01-3c8d227da5ea")]
+[assembly: Guid("0db8cf12-1a92-4607-916d-5926e073af65")]
 
 // Сведения о версии сборки состоят из следующих четырех значений:
 //
